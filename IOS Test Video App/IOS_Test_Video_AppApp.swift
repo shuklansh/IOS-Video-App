9b56b267f4f1d498f0a768cfc46bd02055533f62
shuklansh <ansh.shukla270501@gmail.com>
2024-06-28 19:59:43 +0530

    Initial Commit

---
+++ IOS Test Video App/IOS_Test_Video_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Test_Video_AppApp.swift
+//  IOS Test Video App
+//
+//  Created by Ansh Shukla on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Test_Video_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
